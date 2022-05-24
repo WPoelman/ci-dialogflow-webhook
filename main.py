26171a16f2025de75019d91abb689f3661f0106d
Wessel Poelman <contact+github@wesselpoelman.nl>
2022-05-24 12:15:23 +0200

    update intents and parameters

--- main.py
+++ main.py
@@ -33,101 +33,106 @@ def create_mp3_response(mp3_url: str, book_title: str, icon_url: str = None):
     }
     return result
 
-
-def read_preface_handler(request):
-    # book_title
-
-    return DEFAULT_RESPONSE
-
-
-def books_by_author_handler(request):
-    # author
-
-    return DEFAULT_RESPONSE
-
-
-def look_up_search_word_handler(request):
-    # search_word
-
-    return DEFAULT_RESPONSE
-
-
-def remove_favorite_book_handler(request):
-    # book_title
-
-    return DEFAULT_RESPONSE
-
-
-def play_book_author_handler(request):
-    # author
-
-    return DEFAULT_RESPONSE
-
-
-def play_book_genre_handler(request):
-    # genre
-
-    return DEFAULT_RESPONSE
-
-
-def author_of_book_title_handler(request):
-    # book_title
-
-    return DEFAULT_RESPONSE
-
-
-def present_books_with_genre_handler(request):
-    # genre
-
-    return DEFAULT_RESPONSE
-
-
-def opinion_positive_handler(request):
-    # person
-
-    return DEFAULT_RESPONSE
-
-
-def play_book_title_handler(request):
-    # book_title
-
-    return DEFAULT_RESPONSE
-
-
-def go_to_chapter_handler(request):
-    # chapter_number
-
     # Here we need to extract the information from the request and find the
     # correct information for it.
-    mp3_url = "https://ia800603.us.archive.org/14/items/annakarenina_mas_1202_librivox/annakarenina_001_tolstoy.mp3"
-    icon_url = "https://ia601601.us.archive.org/26/items/LibrivoxCdCoverArt15/anna_karenina_1202.jpg"
-    book_title = "Anna Karenina"
-
-    response = create_mp3_response(mp3_url, book_title, icon_url)
-
-    return response
+# mp3_url = "https://ia800603.us.archive.org/14/items/annakarenina_mas_1202_librivox/annakarenina_001_tolstoy.mp3"
+# icon_url = "https://ia601601.us.archive.org/26/items/LibrivoxCdCoverArt15/anna_karenina_1202.jpg"
+# book_title = "Anna Karenina"
+#
+# response = create_mp3_response(mp3_url, book_title, icon_url)
 
 
-def opinion_negative_handler(request):
-    # character_name
+def books_by_author_handler(payload):
+    # author
+
+    return DEFAULT_RESPONSE
+
+
+def play_book_author_handler(payload):
+    # author
+
+    return DEFAULT_RESPONSE
+
+
+def play_book_genre_handler(payload):
+    # genre
+
+    return DEFAULT_RESPONSE
+
+
+def number_of_chapters_handler(payload):
+    # current_book
+
+    return DEFAULT_RESPONSE
+
+
+def author_of_book_title_handler(payload):
     # book_title
+
+    return DEFAULT_RESPONSE
+
+
+def present_books_with_genre_handler(payload):
+    # genre
+
+    return DEFAULT_RESPONSE
+
+
+def play_book_title_handler(payload):
+    # book_title
+
+    return DEFAULT_RESPONSE
+
+
+def book_genre_handler(payload):
+    # current_book
+
+    return DEFAULT_RESPONSE
+
+
+def go_to_chapter_handler(payload):
     # chapter_number
-    pass
+
+    return DEFAULT_RESPONSE
+
+
+def progress_book_handler(payload):
+    # current_book
+
+    return DEFAULT_RESPONSE
+
+
+def time_to_finish_handler(payload):
+    # current_book
+
+    return DEFAULT_RESPONSE
+
+
+def unread_chapters_handler(payload):
+    # NO PARAMETERS
+
+    return DEFAULT_RESPONSE
+
+
+def available_books_handler(payload):
+    # NO PARAMETERS
+
+    return DEFAULT_RESPONSE
 
 
 INTENTS = {
-    "read_preface": read_preface_handler,
     "books_by_author": books_by_author_handler,
-    "look_up_search_word": look_up_search_word_handler,
-    "remove_favorite_book": remove_favorite_book_handler,
     "play_book_author": play_book_author_handler,
     "play_book_genre": play_book_genre_handler,
+    "number_of_chapters": number_of_chapters_handler,
     "author_of_book_title": author_of_book_title_handler,
     "present_books_with_genre": present_books_with_genre_handler,
-    "opinion_positive": opinion_positive_handler,
     "play_book_title": play_book_title_handler,
+    "book_genre": book_genre_handler,
     "go_to_chapter": go_to_chapter_handler,
-    "opinion_negative": opinion_negative_handler,
+    "progress_book": progress_book_handler,
+    "time_to_finish": time_to_finish_handler,
+    "unread_chapters": unread_chapters_handler,
 }
 
 
